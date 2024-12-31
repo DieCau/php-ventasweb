62e4b1018906faca028bec0dbe759803e6ea3734
Diego Caucota <diegocaucota@gmail.com>
2024-12-30 23:44:51 -0300

    add: controllers and login view

---
+++ app/config.php
@@ -0,0 +1,15 @@
+<<?php 
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'root');
+define('PASSWORD', 'root');
+define('DB', 'db_ventas');
+
+$servidor = "mysql:dbname=".DB.";host=".SERVIDOR;
+
+try {
+  $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+  // echo "Conexión exitosa a la base de datos";
+} catch (PDOException $e) {
+  print_r($e);
+  echo "Error de conexion a la Base de Datos";
+}
